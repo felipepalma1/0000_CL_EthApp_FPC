c1df603ed8b480ea59faed6151a9552381382a81
Felipe Palma <felipepalmacastro@gmail.com>
2020-09-05 13:38:47 -0400

    Primer

---
+++ contracts/ToDoList.sol
@@ -0,0 +1,29 @@
+pragma solidity^0.5.0;
+
+contract ToDoList{
+
+    uint public taskCount = 0;
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    constructor() public {
+        createTask("Crear un proyecto que genere dinero");
+    }
+
+    
+    function createTask(string memory _content) public {
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
+
+
+
+
+}
+
